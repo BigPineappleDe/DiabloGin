4e3680790075f4f111795b722e3038b1e6646c10
BigPineappleDe <544730126@qq.com>
2020-10-12 10:08:58 +0800

    加入线程锁

--- code/Model.go
+++ code/Model.go
@@ -3,20 +3,27 @@ package code
 Mysql支持
  */
 import (
-	_ "github.com/go-sql-driver/mysql"
-	"database/sql"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"diabloGin/config"
+	"sync"
 )
 
-//初始化
-func DbConnect() *sql.DB{
-	connect, err := sql.Open("mysql", config.DBUSERNAME+":"+config.DBPASSWORD+"@tcp("+config.DBHOST+")/"+config.DBNAME)
-	if nil != err {
-		SetLogger("connect db error: "+err.Error(), "error")
-	}
-	return connect
+//防止并发耗费资源 加入线程锁
+var db *gorm.DB
+var once sync.Once
+func GormInit() *gorm.DB {
+	once.Do(func() {
+		var err error
+		conn, err := gorm.Open("mysql", config.DBUSERNAME+":"+config.DBPASSWORD+"@tcp("+config.DBHOST+")/"+config.DBNAME+"?charset=utf8mb4&parseTime=True&loc=Local")
+		if err != nil {
+			fmt.Println(err)
+		}
+		conn.LogMode(true)
+		db=conn
+	})
+	return db
+	//defer conn.Close()
 }
-//表名
-func TableName(tableName string) string {
-	return config.DBPREFIX + tableName
-}
+
--- code/Redis.go
+++ code/Redis.go
@@ -3,20 +3,30 @@ package code
 import (
 	"diabloGin/config"
 	"github.com/go-redis/redis"
+	"sync"
 )
+
 
 /**
 redis核心操作
+防止并发耗费资源 加入线程锁
 */
+var redisDb *redis.Client
+var redisOnce sync.Once
 func RedisInit() *redis.Client{
-	redisClient := redis.NewClient(&redis.Options{
-		Addr: config.REDISHOST,
-		DB:   0, //redis默认有0-15共16个数据库，这里设置操作索引为0的数据库
-	})
-	_,err:=redisClient.Ping().Result()
+	redisOnce.Do(func() {
+		redisClient := redis.NewClient(&redis.Options{
+			Addr: config.REDISHOST,
+			DB:   0, //redis默认有0-15共16个数据库，这里设置操作索引为0的数据库
+		})
+		_,err:=redisClient.Ping().Result()
 
-	if err!=nil{
-		panic(err)
-	}
-	return redisClient
+		if err!=nil{
+			panic(err)
+		}
+
+		redisDb=redisClient
+	})
+
+	return redisDb
 }
